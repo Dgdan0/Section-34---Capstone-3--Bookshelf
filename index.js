f7aa8b6c2ce3e14201dd77f2319ecbd3979f83a2
Dgdan0 <Dgdanbz0@gmail.com>
2024-05-07 21:24:05 +0300

    Version 0.1: Registration works + used added .env to hide DB info

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import axios from "axios";
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
 
 
 
 const app = express();
 const port = 3000;
-
+const saltRounds = 10;
 
 
 //*     Middlewear
 app.use(express.static("public"));      // Main route will be public
 app.use(bodyParser.urlencoded({extended: true}));       // Can use res.body to get json data
 app.use(express.json());
-
-
+dotenv.config();
 
 //*     Database Connection
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "Bookshelf",
-    password: "9706",
-    port: 5433,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: String(process.env.DB_PASSWORD),
+    port: parseInt(process.env.DB_PORT)
   })
 db.connect();
 
@@ -33,15 +34,92 @@ app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
 
-
+//? Homepage
 app.get("/", (req, res) => {
     res.render('index.ejs', {
         activePage: 'home',
     });
 });
 
+//? Register 
+app.get("/register", (req, res) => {
+    res.render('register.ejs', {
+        activePage: 'login',
+    })
+})
+
+//? Check user info (Username and Email) if they exist in Users DB
+app.post("/check-registration", async (req, res) => {
+    const { username, email } = req.body;
+    
+    try {
+        const result = await db.query(`
+            SELECT 
+                (SELECT COUNT(*) FROM users WHERE username = $1) AS username_count,
+                (SELECT COUNT(*) FROM users WHERE email = $2) AS email_count
+        `, [username, email]);
+        
+        const validUser = parseInt(result.rows[0].username_count) === 0;
+        const validEmail = parseInt(result.rows[0].email_count) === 0;
+
+        console.log('Valid User - '+validUser+'\nValid Email - ' + validEmail);
+
+        if (validUser && validEmail) {
+            res.status(200).json({ unique: true, my_message: "Username and Email are unique" });
+        } else if (!validUser && !validEmail) {
+            res.status(400).json({ unique: false, my_message: "Both Username and Email are taken" });
+        } else if (!validUser) {
+            res.status(400).json({ unique: false, my_message: "Username is taken" });
+        } else {
+            res.status(400).json({ unique: false, my_message: "Email is taken" });
+        }
+    } catch (error) {
+        console.error("Database error:", error);
+        res.status(500).json({ unique: false, message: "Database error. Please try again later." });
+    }
+});
+
+//? Registering user into users DB
+app.post("/register", async (req, res) => {
+    const {username, password, email} = req.body
+    console.log('Registering')
+    try{
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const response = await db.query(`
+        INSERT INTO users(username, hashed_password, email) 
+        VALUES($1, $2, $3)
+    `, [username, hashedPassword, email]);
+
+    res.redirect('/login?success=1');
+
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({ message: "Error registering user" });
+    }
+})
+
+app.get("/login", (req, res) => {
+    res.render('login.ejs', {
+        activePage: 'login',
+        success: req.query.success,
+    });
+});
+
+app.post("/login", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+
+
+    res.render('login.ejs', {
+        activePage: 'login',
+    })
+});
+
+
 app.get("/add", (req, res) => {
-    res.render('index.ejs', {
+    res.render('add.ejs', {
         activePage: 'add',
     });
 });
--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,7 @@
+import axios from 'https://cdn.skypack.dev/axios';
+
+
+
 //!     Sidebar eventlistener for clicked effect
 const sidebarItems = document.querySelectorAll('#sidebar-container ul li');
 sidebarItems.forEach(item => {
@@ -10,3 +14,86 @@ sidebarItems.forEach(item => {
         item.classList.add('sidebar-clicked');
     });
 });
+
+//! Tooltip requirment + check if can send form
+var form = document.querySelector('form');
+var inputs = document.querySelectorAll('.input-box input');
+
+form.onsubmit = async function(e) {
+    e.preventDefault();
+    var allValid = true;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    inputs.forEach(function(input) {
+        var tooltip = input.nextElementSibling;
+        var listItems = tooltip.querySelectorAll('li');
+        var value = input.value;
+
+        if (input.name === 'password' || input.name === 'username'){
+            listItems[0].style.color = (value.length >= 6 && value.length <= 15) ? 'black' : 'red';
+        }
+        if (input.name === 'password'){
+            listItems[1].style.color = /[a-z]/.test(value) ? 'black' : 'red';
+            listItems[2].style.color = /[A-Z]/.test(value) ? 'black' : 'red';
+            listItems[3].style.color = /\d/.test(value) ? 'black' : 'red';
+        }
+        if (input.name === 'email'){
+            listItems[0].style.color = emailPattern.test(value) ? 'black' : 'red';
+        }
+        // Check if any list item is red
+        var inputValid = true;
+        for (var i = 0; i < listItems.length; i++) {
+            if (listItems[i].style.color === 'red') {
+                inputValid = false;
+                break;
+            }
+        }
+        // If input is not valid, add shake class to tooltip
+        if (!inputValid) {
+            tooltip.style.display = "block";
+            tooltip.classList.add('shake');
+            // Remove shake class after animation ends
+            setTimeout(function() {
+                tooltip.classList.remove('shake');
+            }, 500);
+            allValid = false;
+        }
+
+    });
+    // Check if user and email exist
+    if(allValid){
+        await checkUniqueness(e);
+    }
+};
+
+async function checkUniqueness(e) {
+    try {
+        let response = await axios.post("/check-registration", {
+            username: form.username.value,
+            email: form.email.value
+        });
+
+        if (response.data.unique) {
+            form.submit(); // Submit the form
+        }
+    } catch (error) {
+        if (error.response && error.response.data && error.response.data.my_message) {
+            alert(error.response.data.my_message);
+        } else {
+            alert("An error occurred. Please try again later.");
+        }
+    }
+}
+
+
+//! Show Hide Tooltip 
+inputs.forEach(function(input) {
+    var tooltip = input.nextElementSibling;
+    input.onfocus = function() {
+        tooltip.style.display = "block";
+    }
+    input.onblur = function() {
+        tooltip.style.display = "none";
+    }
+});
+
